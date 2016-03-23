7cb7a11021dd7969c9e05a2db268da734fd74aa0
befey <t431962@LDIMAC135F8J9.local>
2016-03-23 13:04:40 -0400

    fixed remove old files installer script fails when file is missing

--- Safeguard Plugins Installer/remove old files.sh
+++ Safeguard Plugins Installer/remove old files.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #Clean up some old files
-rm -R /Applications/Adobe\ Illustrator\ CC\ 2015/Plug-ins.localized/ColorTools.aip
-rm -R /Applications/Adobe\ Illustrator\ CC\ 2015/Plug-ins.localized/TextTools.aip
+rm -Rf /Applications/Adobe\ Illustrator\ CC\ 2015/Plug-ins.localized/ColorTools.aip
+rm -Rf /Applications/Adobe\ Illustrator\ CC\ 2015/Plug-ins.localized/TextTools.aip
