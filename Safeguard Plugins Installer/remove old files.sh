08a3d7cda18e55a0dbc448dfe9afa0c945870c64
befey <t431962@LDIMAC135F8J9.local>
2016-03-24 14:53:00 -0400

    Renamed Plugin Safeguard Tools and moved all files to new directory outside SDK

--- Safeguard Plugins Installer/remove old files.sh
+++ Safeguard Plugins Installer/remove old files.sh
@@ -3,3 +3,7 @@
 #Clean up some old files
 rm -Rf /Applications/Adobe\ Illustrator\ CC\ 2015/Plug-ins.localized/ColorTools.aip
 rm -Rf /Applications/Adobe\ Illustrator\ CC\ 2015/Plug-ins.localized/TextTools.aip
+
+#3/24/16 -- Renamed to SafeguardTools and consolidated all plugins to one file
+rm -Rf /Applications/Adobe\ Illustrator\ CC\ 2015/Plug-ins.localized/Safeguard/ColorTools.aip
+rm -Rf /Applications/Adobe\ Illustrator\ CC\ 2015/Plug-ins.localized/Safeguard/TextTools.aip
