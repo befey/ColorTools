b3e685ed7698bc03ac89721898f45a21e77ced0a
befey <t431962@LDIMAC135F8J9.local>
2016-04-08 13:58:00 -0400

    Cleaned up includes, added path macros

--- Source/PrintToPdf.h
+++ Source/PrintToPdf.h
@@ -9,20 +9,20 @@
 #ifndef __SafeguardTools__PrintToPdf__
 #define __SafeguardTools__PrintToPdf__
 
-#include "AIDocumentList.h"
 #include "AIActionManager.h"
 #include "VPB.h"
 #include "VPBFactory.h"
 #include "PlateNumber.h"
 
 #define PATH_TO_PLANT_MANUFACTURING	"/Volumes/Plant_Manufacturing"
+#define PATH_TO_PDFPROOFS "/Volumes/PDFProofs"
+#define PATH_TO_MICR_PDF "/Volumes/MICR_PDF"
 #define DEFAULT_OUTPUTPATH "/Users/t431962/Desktop/WORKING"
 
 #define MANUFACTURING_PDF_PRESET    "Manufacturing"
 #define MICR_PROOF_PDF_PRESET       "MICR"
 #define REG_PROOF_PDF_PRESET        "Proof"
 
-extern "C" AIDocumentListSuite* sAIDocumentList;
 extern "C" AIActionManagerSuite* sAIActionManager;
 
 bool PrintToPdf();
