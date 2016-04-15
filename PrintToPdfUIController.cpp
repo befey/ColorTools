d8c78cc19de8a3cc597e17bf8672c193da276f6d
befey <t431962@LDIMAC135F8J9.local>
2016-04-15 12:56:39 -0400

    Created Print to PDF UI Controller

---
+++ PrintToPdfUIController.cpp
@@ -0,0 +1,60 @@
+//
+//  PrintToPdfUIController.cpp
+//  SafeguardTools
+//
+//  Created by T431962 on 4/15/16.
+//
+//
+
+#include "PrintToPdfUIController.h"
+
+PrintToPdfUIController::PrintToPdfUIController(void)
+: FlashUIController(PRINTTOPDF_UI_EXTENSION)
+{
+}
+
+
+/* Add event listeners
+ */
+csxs::event::EventErrorCode PrintToPdfUIController::RegisterCSXSEventListeners()
+{
+    csxs::event::EventErrorCode result = csxs::event::kEventErrorCode_Success;
+    do {
+        //result =  fPPLib.AddEventListener(EVENT_TYPE_CHANGE_CLICKED, ChangeButtonClickedFunc, this);
+        //if (result != csxs::event::kEventErrorCode_Success)
+        //{
+        //    break;
+        //}
+    }
+    while(false);
+    return result;
+}
+
+/* Remove event listeners
+ */
+csxs::event::EventErrorCode PrintToPdfUIController::RemoveEventListeners()
+{
+    csxs::event::EventErrorCode result = csxs::event::kEventErrorCode_Success;
+    do {
+        //result =  fPPLib.RemoveEventListener(EVENT_TYPE_CHANGE_CLICKED, ChangeButtonClickedFunc, this);
+        //if (result != csxs::event::kEventErrorCode_Success)
+        //{
+        //    break;
+        //}
+    }
+    while(false);
+    return result;
+}
+
+ASErr PrintToPdfUIController::SendData()
+{
+    AIErr error = kNoErr;
+    return error;
+}
+
+/* Parse received data from flash panel.
+ */
+void PrintToPdfUIController::ParseData(const char* eventData)
+{
+    return;
+}
---
+++ PrintToPdfUIController.h
@@ -0,0 +1,43 @@
+//
+//  PrintToPdfUIController.h
+//  SafeguardTools
+//
+//  Created by T431962 on 4/15/16.
+//
+//
+
+#ifndef __SafeguardTools__PrintToPdfUIController__
+#define __SafeguardTools__PrintToPdfUIController__
+
+#include "SDKPlugPlug.h"
+#include "FlashUIController.h"
+
+#define PRINTTOPDF_UI_EXTENSION             "com.gosafeguard.PrintToPdf"
+#define ILST_APP                            "ILST"
+
+class SafeguardToolsPlugin;
+extern SafeguardToolsPlugin *gPlugin;
+
+class PrintToPdfUIController : public FlashUIController
+{
+public:
+    PrintToPdfUIController(void);
+    
+    /** Registers the events this plug-in will listen for with PlugPlug.
+     @return error code if error found, or success otherwise.
+     */
+    csxs::event::EventErrorCode RegisterCSXSEventListeners();
+    
+    /** Removes the previously added event listeners from PlugPlug.
+     @return error code if error found, or success otherwise.
+     */
+    csxs::event::EventErrorCode RemoveEventListeners();
+    
+    ASErr SendData();
+    
+    void ParseData(const char* eventData);
+        
+private:
+};
+
+#endif /* defined(__SafeguardTools__PrintToPdfUIController__) */
