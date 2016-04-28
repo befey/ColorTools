86da59d5c57ff79c8be1ee792e1a891af69bb912
befey <t431962@LDIMAC135F8J9.local>
2016-04-28 14:51:53 -0400

    Created PdfResults

---
+++ PdfResults.cpp
@@ -0,0 +1,32 @@
+//
+//  PdfResults.cpp
+//  SafeguardTools
+//
+//  Created by T431962 on 4/27/16.
+//
+//
+
+#include "PdfResults.h"
+
+
+string PdfResults::MakeXmlString() const
+{
+    //Format as XML string
+    string xmlString;
+    
+    xmlString.append("<root>");
+    for (auto it : results)
+    {
+        if (it.action == Transaction::Action::Created)
+        {
+            xmlString.append("<create>").append(it.path.GetFullPath().getInStdString(kAIPlatformCharacterEncoding)).append("</create>");
+        }
+        else if (it.action == Transaction::Action::Deleted)
+        {
+            xmlString.append("<delete>").append(it.path.GetFullPath().getInStdString(kAIPlatformCharacterEncoding)).append("</delete>");
+        }
+    }
+    xmlString.append("</root>");
+    
+    return xmlString;
+}
---
+++ PdfResults.h
@@ -0,0 +1,31 @@
+//
+//  PdfResults.h
+//  SafeguardTools
+//
+//  Created by T431962 on 4/27/16.
+//
+//
+
+#ifndef __SafeguardTools__PdfResults__
+#define __SafeguardTools__PdfResults__
+
+class PdfResults
+{
+public:
+    struct Transaction
+    {
+        enum Action
+        {
+            Deleted,
+            Created
+        };
+        Action action;
+        ai::FilePath path;
+    };
+    
+    std::vector<Transaction> results;
+    
+    string MakeXmlString() const;
+};
+
+#endif /* defined(__SafeguardTools__PdfResults__) */
