ee6b56a29325ccfd64f06fd21b4da86735c7bf95
befey <t431962@LDIMAC135F8J9.local>
2016-10-11 14:57:35 -0400

    Pulled all dictionary stuff out

--- Source/PlateBleedInfo/BleedInfoDrawer.h
+++ Source/PlateBleedInfo/BleedInfoDrawer.h
@@ -31,13 +31,13 @@ namespace SafeguardFile
         
         BleedInfoDrawer& AddDrawer(shared_ptr<IDrawer> val) { drawers.push_back(val); return *this; };
 
-        void Remove(AIArtHandle pluginGroupArt) const;
+        void Remove(AIArtHandle& pluginGroupArt) const;
     private:
         ai::ArtboardID artboardIndex;
         
         vector<shared_ptr<IDrawer>> drawers;
         
-        AIArtHandle DoDraw(AIArtHandle resultGroup = NULL) const override;
+        AIArtHandle DoDraw(AIArtHandle pluginGroupArt = NULL) const override;
         
         AIArtHandle Add() const;
         AIArtHandle Update(AIArtHandle pluginGroupArt) const;
