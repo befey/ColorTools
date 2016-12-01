35b8b4e736466c10ba6de00c5a6388d82f33e2a6
befey <t431962@LDIMAC135F8J9.local>
2016-12-01 15:25:47 -0500

    Toggle all artboards on when proof is selected

--- com.gosafeguard.SafeguardTools/js/PrintToPdf.js
+++ com.gosafeguard.SafeguardTools/js/PrintToPdf.js
@@ -24,6 +24,7 @@ $(function()
                              else
                              {
                              $('#separatefiles-check').prop('checked', false);
+                             ToggleArtboardPrint($('.artboard-deselected'));
                              }
                              });
   
